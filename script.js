6d04add32097bac875f901976c5f1ca8b8ce0eaf
hyedevj <hyedevj@gmail.com>
2023-09-12 14:37:55 +0900

    calculator class 만들기

---
+++ script.js
@@ -0,0 +1,12 @@
+class Calculator {
+
+}
+const cal = new Calculator()
+
+const $previousPreview = document.querySelector('[data-previous-preview]')
+const $currentPreview = document.querySelector('[data-current-preview]')
+
+
+// ES6 이전 객체 생성 방법
+// function Calculator() { }
+// const cal = new Calculator()
