8dcc1900c3c9aab6c75ea6df656518662abe0bcf
hyedevj <hyedevj@gmail.com>
2023-09-12 14:59:43 +0900

    계산기 기능 관련 DOM 셋팅]

--- script.js
+++ script.js
@@ -3,10 +3,28 @@ class Calculator {
 }
 const cal = new Calculator()
 
+// 값 표시
 const $previousPreview = document.querySelector('[data-previous-preview]')
 const $currentPreview = document.querySelector('[data-current-preview]')
 
+// 사칙 연산
+const $plus = document.querySelector('[data-btn-plus]')
+const $minus = document.querySelector('[data-btn-minus]')
+const $multiply = document.querySelector('[data-btn-multiply]')
+const $divide = document.querySelector('[data-btn-divide]')
 
-// ES6 이전 객체 생성 방법
-// function Calculator() { }
-// const cal = new Calculator()
+// 숫자, 연산
+const $numbers = document.querySelectorAll('[data-btn-number]')
+const $operations = document.querySelectorAll('[data-btn-operation]')
+
+$numbers.forEach(($number) => {
+  $number.addEventListener("click", (e) => {
+    console.log(e.target.textContent)
+  })
+})
+
+$operations.forEach(($operation) => {
+  $operation.addEventListener("click", (e) => {
+    console.log(e.target.textContent)
+  })
+})
