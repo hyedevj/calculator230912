f8dc264a7c3eb8ca8ce128b298f4391cf99fcda4
hyedevj <127413734+hyedevj@users.noreply.github.com>
2023-09-15 20:26:17 +0900

    onEqual 기능 구현

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 class Calculator {
   $previousPreview
   $currentPreview
+  previousOperation = ""
+  currentOperation = ""
 
   // 생성자 함수 : 클래스가 생성될 때 실행되는 자동 실행 함수
   constructor($previousPreview, $currentPreview) {
@@ -11,7 +13,6 @@ class Calculator {
   onPressNumber(number) {
     // 벨리데이션: 데이터를 검진. 불필요한 것 막아주기
     // 소수점 입력시 현재 입력 받은 데이터가 없는 경우
-    console.log(number)
     if(number === "." && this.$currentPreview.textContent.length < 1) {
       return
     }
@@ -23,9 +24,45 @@ class Calculator {
     if(this.$currentPreview.textContent.length < 1) {
       return
     }
-
+    this.previousOperation = operation
     this.$previousPreview.textContent = `${this.$currentPreview.textContent} ${operation}`
     this.$currentPreview.textContent = ''
+  }
+
+  onEqual() {
+    if(
+      this.$currentPreview.textContent.length < 1 ||
+      this.$previousPreview.textContent.length < 1 ||
+      this.previousOperation.length < 1
+    ) {
+      return
+    }
+
+    let result = 0
+    
+    switch (this.previousOperation) {
+      case "+":
+        result = Number(this.$previousPreview.textContent.split(" ")[0])
+        + Number(this.$currentPreview.textContent)
+        break;
+        case "-":
+          result = Number(this.$previousPreview.textContent.split(" ")[0])
+          - Number(this.$currentPreview.textContent)
+        break;
+        case "*":
+          result = Number(this.$previousPreview.textContent.split(" ")[0])
+          * Number(this.$currentPreview.textContent)
+        break;
+        case "÷":
+          result = Number(this.$previousPreview.textContent.split(" ")[0])
+          / Number(this.$currentPreview.textContent)
+        break;
+      default:
+        break;
+    }
+    this.$currentPreview.textContent = result.toString()
+    this.$previousPreview.textContent = ""
+    this.currentOperation = ""
   }
 }
 
@@ -68,7 +105,7 @@ $operations.forEach(($operation) => {
         cal.onPressOperation("÷")
         break;
       case $equal:
-        // cal.onPressOperation("=")
+        cal.onEqual()
         break;
       default:
         break;
